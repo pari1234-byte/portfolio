c45ed3757448a25fe8de1bc5c016c4fa4aa0148a
pari1234-byte <safinazk41@gmail.com>
2026-05-24 11:15:54 -0700

    fixed github pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./index.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import NavBar from "./Components/NavBar";
 
